2d84243738ac7cecbfd3baaef2519521ad312d91
Nate <nate@natesoares.com>
2011-01-11 19:36:40 -0500

    stable

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject one "0.0.1-SNAPSHOT"
+(defproject one "1"
   :description "Nate Soares Project 1"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
--- src/one/core.clj
+++ src/one/core.clj
@@ -6,16 +6,12 @@
   (:use clojure.set)
   (:gen-class))
 
-; Utility functions
-(defn any [lst] (first (filter identity lst)))
-(defn snap [x n] (- x (mod x n)))
-(defn vecmap [f a] (vec (map f a)))
-
 (defn input [file]
   (let [raw (io/read-lines file)
-        lists (map #(string/split #"\s" %) raw)
-        convert #(if (= % "-") 0 (Integer/parseInt %))]
-    (vecmap #(vecmap convert %) lists)))
+        zeroed (map #(string/replace-char \- \0 %) raw)
+        words (map #(string/split #"\s" %) zeroed)
+        data (map (partial map #(Integer/parseInt %)) words)]
+    (vec (map vec data))))
 
 (defn solve [grid]
 
@@ -32,7 +28,8 @@
     ; Grid traversal
     (nth-col [grid i] (take-nth side (drop i (apply concat grid))))
     (coord-square [grid r c]
-      (let [i (snap r n), j (snap c n)]
+      (let [i (- r (mod r n))
+            j (- c (mod c n))]
         (for [x (range n) y (range n)]
           (nth (nth grid (+ i x)) (+ y j)))))
     
@@ -43,23 +40,22 @@
       (count (zip/lefts (zip/up loc)))
       (count (zip/lefts loc))))
 
-    ; Search functions
-    (options [loc]
-      (difference nums (set (union (row-of loc) (col-of loc) (sqr-of loc)))))
+    ; Zipper searching
+    (taken [loc] (set (union (row-of loc) (col-of loc) (sqr-of loc))))
+    (options [loc] (difference nums (taken loc)))
     (fill [loc] (map #(zip/next (zip/replace loc %)) (options loc)))
     (search [loc]
       (cond
         (zip/end? loc) (zip/root loc)
         (vector? (zip/node loc)) (search (zip/next loc))
         (pos? (zip/node loc)) (search (zip/next loc))
-        :else (any (map search (fill loc)))))]
+        :else (some identity (map search (fill loc)))))]
 
   ; Ensure that the grid is well-formed
   (assert (integer? n))           ; The input is a valid length
   (assert (apply = counts))       ; Each row is the same length
   (assert (= side (count grid)))  ; The input is square
 
-  ; Solve the grid
   (search (zip/vector-zip grid)))))
 
 (defn -main [file] (dorun (map println (solve (input file)))))
